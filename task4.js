aeae1c00849e440ad33003a0d990de9bfbe3ee51
albertr808 <baltazarramirez506@gmail.com>
2025-09-05 14:36:10 -0600

    ejercicios resueltos

--- task4.js
+++ task4.js
@@ -1,7 +1,20 @@
 export class Player {
-    constructor() {
-      
-    }
-  
-    
+  constructor(name, level) {
+    this.name = name
+    this.level = level
   }
+
+  info() {
+    console.log(`${this.name} has reached Level ${this.level}!`)
+  }
+
+  levelUp() {
+    this.level += 1
+  }
+}
+
+const player1 = new Player("Tara", 6)
+
+player1.info() 
+player1.levelUp()  
+player1.info()
